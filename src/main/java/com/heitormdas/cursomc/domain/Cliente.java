03c3b0a4fb4fdec8bece1f9ed4f3a27490d844a8
Heitor Moura <heitormoura2@gmail.com>
2018-04-06 21:21:41 -0300

    Consertado erros do ultimo commit

--- src/main/java/com/heitormdas/cursomc/domain/Cliente.java
+++ src/main/java/com/heitormdas/cursomc/domain/Cliente.java
@@ -42,31 +42,6 @@ public class Cliente implements Serializable {
 	public Cliente() {
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
 		this.id = id;
@@ -132,7 +107,6 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 	
-<<<<<<< HEAD
 	public List<Pedido> getPedidos() {
 		return pedidos;
 	}
@@ -166,7 +140,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-=======
->>>>>>> parent of 83b80d6... Endpoint /clientes/{id} disponivel
 	
 }
